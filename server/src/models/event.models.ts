f032ef797f15c08dcef21b4423f8ed5aee44571e
doudaoud <boudaoudreda1@gmail.com>
2026-05-03 00:55:02 +0100

    Refactor event model: update organizer field to use Organizateur type and add Organizer type definition

--- server/src/models/event.models.ts
+++ server/src/models/event.models.ts
@@ -1,11 +1,15 @@
-import { Schema as schema, model, Document } from "mongoose";
-
+import { Schema as schema, Document, ObjectId } from "mongoose";
 export enum EventType {
   CONFERENCE = "conference",
   WORKSHOP = "workshop",
   HACKATHON = "hackathon",
 }
-
+//  cest un objet qui va faire reference a l'id de l'organisateur de user direct pour lie l'event a son organisateur sans faire tout ca dans le schema
+export type Organizateur = {
+  type: ObjectId;
+  ref: "User";
+  required: true;
+};
 export interface IEvent extends Document {
   title: string;
   description: string;
@@ -13,11 +17,22 @@ export interface IEvent extends Document {
   date_End: Date;
   location: string;
   type: EventType;
-  organizer: string; // pour lie l"organizateur de l'event
+  // organizer: string; // pour lie l"organizateur de l'event
+  organizer: Organizateur;
   createdAt: Date;
   updatedAt: Date;
 }
 
 export const eventSchema = new schema<IEvent>({
   title: { type: String, required: true, minlength: 4, maxLength: 100 },
+  description: { type: String, required: true, minlength: 10, maxLength: 1000 },
+  date_Begin: { type: Date, required: true },
+  date_End: { type: Date, required: true },
+  location: { type: String, required: true },
+  type: {
+    type: String,
+    enum: EventType,
+    required: true,
+  },
+  organizer: { type: schema.Types.ObjectId, ref: "Organizer", required: true },
 });
