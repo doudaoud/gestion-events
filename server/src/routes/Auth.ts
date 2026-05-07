d00ab58059a0bf36b184d56cfa20d620ec197402
doudaoud <boudaoudreda1@gmail.com>
2026-05-07 21:56:53 +0100

    Add bcrypt for password hashing and update authentication route to verify user credentials

--- server/src/routes/Auth.ts
+++ server/src/routes/Auth.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import mongoose from "mongoose";
-import 
+import { User } from "../models/user.models.js";
+import byctypt from "bcryptjs";
 const AuthRouter: express.Router = express.Router();
 /**
  * @method POST
@@ -12,10 +13,22 @@ const AuthRouter: express.Router = express.Router();
 
 AuthRouter.post(
   "/Login",
-  expressAsyncHandler(
-      async (req: express.Request, res: express.Response) => {
-          const clientInfoLogin = req.body
-          const userexist = await 
-    },
-  ),
+  expressAsyncHandler(async (req: express.Request, res: express.Response) => {
+    const clientInfoLogin = req.body;
+    const userexist = await User.findOne({
+      email: clientInfoLogin.email,
+    }).select("+passwordHash");
+    if (!userexist) {
+      res.status(404).json({ message: "Utilisateur non trouvé" });
+      return;
+    }
+    const password_correct: boolean = await byctypt.compare(
+      clientInfoLogin.password,
+      userexist.passwordHash,
+    );
+    if (!password_correct) {
+      res.status(401).json({ message: "mot de passe incorrect" });
+      return;
+    }
+  }),
 );
