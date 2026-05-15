42f18c344f1183416905165bf16a396135fcc35c
doudaoud <boudaoudreda1@gmail.com>
2026-05-15 19:12:27 +0100

    Refactor code structure for improved readability and maintainability

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import logo from "../../assets/screen.png";
 
-export default function Home() {
+export default function Home() : React.ReactNode {
   return (
     <React.Fragment>
-      <h1>this is the home page the page of accueil</h1>
+      {/* navbar */}
+      <nav>
+        <div className="left">
+          <img src={logo} alt="logo de notre app " />
+        </div>
+        <div className="middle"></div>
+        <div className="right"></div>
+      </nav>
+      {/* navbar */}
     </React.Fragment>
   );
 }
