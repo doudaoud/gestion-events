95f948ecfcc6c09d399088f6fa8dd18120763cc3
doudaoud <boudaoudreda1@gmail.com>
2026-05-21 00:51:16 +0100

    fix: remove smooth scroll behavior and hide scrollbar in App.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "../src/pages/Home";
-
+import "./App.css";
 export default function App(): React.ReactElement {
   return (
     <React.Fragment>
