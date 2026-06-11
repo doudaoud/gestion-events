01e9bb1be67fb91222bacda60ae05c6af65d9d1a
doudaoud <boudaoudreda1@gmail.com>
2026-06-11 19:17:17 +0100

    feat: enhance Verificationcode component layout and styling

--- client/src/pages/Verificationcode.tsx
+++ client/src/pages/Verificationcode.tsx
@@ -3,10 +3,39 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 export default function Verificationcode() {
   return (
     <>
-      <div className="verification-form">
-        <div className="check-icon">
-            <CheckCircleIcon/>
+      <div
+        className="verification-form"
+        style={{
+          display: "flex",
+          flexFlow: "column wrap",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <div
+          className="check-icon"
+          style={{
+            backgroundColor: "rgb(99, 102, 241)",
+            // borderRadius: "50%",
+            height: "40px",
+            width: "40px",
+            borderRadius: "10px",
+          }}
+        >
+          <CheckCircleIcon
+            style={{
+              color: "white",
+              marginLeft: "7px",
+              marginTop: "6px",
+              //   fontWeight:"bold",
+              //   height:"30px"
+            }}
+          />
         </div>
+        <h1>
+            Gestion Events
+        </h1>
       </div>
     </>
   );
